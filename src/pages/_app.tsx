c46992aae9da0519bc071b46213abad19a87a4d0
Berni <behausleitner@gmail.com>
2024-05-30 10:55:26 +0200

    move head to _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,7 @@ import { api } from "~/utils/api";
 import "~/styles/globals.css";
 import { PageLayout } from "~/components/layout/layout";
 import AuthenticationPage from "~/components/layout/auth-landing";
-
+import Head from "next/head";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
 
@@ -16,6 +16,11 @@ const MyApp: AppType = ({ Component, pageProps }) => {
           footer: "hidden",
         }
       }}>
+        <Head>
+          <title>Soff</title>
+          <meta name="description" content="Built somewhere inbetween Hayes & Glen Park" />
+          <link rel="icon" href="/beaver.ico" />
+        </Head>
         <SignedOut>
           <AuthenticationPage />
         </SignedOut>
