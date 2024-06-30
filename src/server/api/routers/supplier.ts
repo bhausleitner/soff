1d6e5220a761d3ef1ca187e641d7a5d0cfcd6ea5
Berni <behausleitner@gmail.com>
2024-06-29 17:08:02 -0700

    sendgrid v0

--- src/server/api/routers/supplier.ts
+++ src/server/api/routers/supplier.ts
@@ -1,6 +1,10 @@
 import { z } from "zod";
 import { Status } from "@prisma/client";
+import sgMail from "@sendgrid/mail";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+
+// init sendgrid mail client
+// sgMail.setApiKey(process.env.SENDGRID_API_KEY!);
 
 // define schema
 const supplierSchema = z.object({
@@ -27,6 +31,23 @@ export const supplierRouter = createTRPCRouter({
     // validate data using schema
     supplierArraySchema.parse(supplierData);
 
+    // // send supplier db
+
+    // const msg = {
+    //   to: "behausleitner@gmail.com", // Change to your recipient
+    //   from: "berni@soff.ai", // Change to your verified sender
+    //   subject: "Hello From Soff!",
+    //   text: "and easy to do anywhere, even with Node.js",
+    //   html: "<strong>and easy to do anywhere, even with Soff ;-)</strong>"
+    // };
+    // sgMail
+    //   .send(msg)
+    //   .then(() => {
+    //     console.log("Email sent");
+    //   })
+    //   .catch((error) => {
+    //     console.error(error);
+    //   });
     // return
     return supplierData;
   })
