3d3f654393a78edfbbe240e72a67d267466f84d5
raban <rabanspiegel@gmail.com>
2024-07-02 17:52:41 -0700

    removed unneccessary migrations

--- prisma/migrations/20240702231309_add_supplier_r_time/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `response_time` to the `Supplier` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Supplier" ADD COLUMN     "response_time" INTEGER NOT NULL;
