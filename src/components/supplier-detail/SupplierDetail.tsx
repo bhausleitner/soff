6466c1f153e00f643aad1354920195c205045b1f
raban <rabanspiegel@gmail.com>
2024-07-03 11:11:10 -0700

    displays supplier info by ID

--- src/components/supplier-detail/SupplierDetail.tsx
+++ src/components/supplier-detail/SupplierDetail.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
 import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
+import { api } from "~/utils/api";
 
-export function SupplierDetail() {
+interface SupplierDetailProps {
+  supplierId: number;
+}
+
+export function SupplierDetail({supplierId}: SupplierDetailProps) {
+  const { data, isLoading, error } = api.supplier.getSupplierById.useQuery({ supplierId});
+
+   if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error.message}</p>;
+  }
+
+  if (!data) {
+    return <p>No supplier found.</p>;
+  }
+
+
   return (
     <>
       <Card>
@@ -11,9 +31,10 @@ export function SupplierDetail() {
           </CardTitle>
         </CardHeader>
         <CardContent>
-          <p className="text-xs text-muted-foreground">
-            Testing this
-          </p>
+          <p className="text-xs text-muted-foreground">Name: {data.title}</p>
+          <p className="text-xs text-muted-foreground">Email: {data.email}</p>
+          <p className="text-xs text-muted-foreground">Status: {data.status}</p>
+          <p className="text-xs text-muted-foreground">Response Time: {data.response_time ?? "N/A"}</p>
         </CardContent>
       </Card>
     </>
