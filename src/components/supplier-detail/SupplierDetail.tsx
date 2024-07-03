cbc734e17cd07ad537ab74db951fa2ec0ed43097
raban <rabanspiegel@gmail.com>
2024-07-02 19:50:15 -0700

    started on SOF-13

---
+++ src/components/supplier-detail/SupplierDetail.tsx
@@ -0,0 +1,11 @@
+import { Card } from "~/components/ui/card";
+
+export function SupplierDetail() {
+  return (
+    <Card>
+      <div className="flex items-center justify-between space-y-2">
+        <h2 className="text-3xl font-bold tracking-tight">Supplier Detail </h2>
+      </div>
+    </Card>
+  );
+}
