f1563fe5070bb0d101700234a3dcc652f1e3fbad
raban <rabanspiegel@gmail.com>
2024-07-03 18:11:25 -0700

    added shadcn breadcrumb + deleted supplier dummy data

--- src/pages/suppliers/[supplierID].tsx
+++ src/pages/suppliers/[supplierID].tsx
@@ -1,13 +1,19 @@
-
 import { useRouter } from "next/router";
 import React from "react";
 import { SupplierDetail } from "../../components/supplier-detail/SupplierDetail";
 
-
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator
+} from "~/components/ui/breadcrumb";
 
 const SupplierPage = () => {
   const router = useRouter();
-  const {supplierId} = router.query;
+  const { supplierId } = router.query;
 
   if (!supplierId) {
     return <p>Loading...</p>;
@@ -18,11 +24,25 @@ const SupplierPage = () => {
   return (
     <>
       <div className="flex-1 space-y-4 p-4 pt-6 md:p-8">
+        <Breadcrumb>
+          <BreadcrumbList>
+            <BreadcrumbItem>
+              <BreadcrumbLink href="/">Home</BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <BreadcrumbLink href="/components">Suppliers</BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <BreadcrumbPage>Breadcrumb</BreadcrumbPage>
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
         <SupplierDetail supplierId={supplierID} />
       </div>
     </>
-        
   );
-}
+};
 
 export default SupplierPage;
