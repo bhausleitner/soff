9e70a1f6e1d23ed231012f7bc27004ff07755ac6
Berni <behausleitner@gmail.com>
2024-07-19 16:55:19 -0700

    Drop user field

---
+++ prisma/migrations/20240719235447_drop_clerk_id_field/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `clerk_user_id` on the `User` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "clerk_user_id";
