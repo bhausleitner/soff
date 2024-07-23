abe1bdccddb9cd516f332ad097bed33ed06d168b
Berni <behausleitner@gmail.com>
2024-07-23 16:12:06 -0700

    Scroll to latest message smoothly

--- src/components/chat/chat-list.tsx
+++ src/components/chat/chat-list.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import { cn } from "~/lib/utils";
 import { type ChatMessage } from "~/server/api/routers/chat";
 import { map } from "lodash";
 import { Icons } from "~/components/icons";
+import { formatDate } from "~/utils/time";
+import { AnimatePresence, motion } from "framer-motion";
 
 interface ChatListProps {
   chatMessages?: ChatMessage[];
@@ -10,51 +12,69 @@ interface ChatListProps {
 }
 
 export function ChatList({
-  chatMessages,
+  chatMessages = [],
   chatParticipantUserId
 }: ChatListProps) {
-  const formatDate = (dateString: string) => {
-    const date = new Date(dateString);
-    const options: Intl.DateTimeFormatOptions = {
-      day: "2-digit",
-      month: "short",
-      year: "numeric",
-      hour: "2-digit",
-      minute: "2-digit"
-    };
-    return date.toLocaleDateString("en-GB", options);
-  };
+  const messagesContainerRef = useRef<HTMLDivElement>(null);
+  const lastMessageRef = useRef<HTMLDivElement>(null);
 
-  // render message left side if chatparticipant is not the user
+  useEffect(() => {
+    if (lastMessageRef.current) {
+      lastMessageRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [chatMessages]);
+
   return (
     <div className="flex-1 overflow-y-auto">
-      <div className="flex-grow overflow-x-hidden">
-        {map(chatMessages, (chatMessage, index) => (
-          <div
-            key={index}
-            className={cn(
-              "flex flex-col gap-2 whitespace-pre-wrap p-4",
-              chatMessage.chatParticipantId === chatParticipantUserId
-                ? "items-end"
-                : "items-start"
-            )}
-          >
-            <div
-              className={cn(
-                "flex flex-col rounded-md p-3",
-                chatMessage.chatParticipantId === chatParticipantUserId
-                  ? "bg-blue-100"
-                  : "bg-accent"
-              )}
-            >
-              <span>{chatMessage.content}</span>
-              <span className="mt-1 flex items-center gap-1 text-xs text-gray-600">
-                <Icons.checkcheck className="h-4 w-4" />
-                {formatDate(chatMessage.createdAt.toString())}
-              </span>
-            </div>
-          </div>
-        ))}
+      <div ref={messagesContainerRef} className="flex-grow overflow-x-hidden">
+        <AnimatePresence>
+          {map(chatMessages, (chatMessage, index) => {
+            const isLastMessage = index === chatMessages.length - 1;
+            return (
+              <motion.div
+                key={index}
+                ref={isLastMessage ? lastMessageRef : null}
+                layout
+                initial={{ opacity: 0, scale: 1, y: 50, x: 0 }}
+                animate={{ opacity: 1, scale: 1, y: 0, x: 0 }}
+                exit={{ opacity: 0, scale: 1, y: 1, x: 0 }}
+                transition={{
+                  opacity: { duration: 0.1 },
+                  layout: {
+                    type: "spring",
+                    bounce: 0.3,
+                    duration: chatMessages.indexOf(chatMessage) * 0.05 + 0.2
+                  }
+                }}
+                style={{
+                  originX: 0.5,
+                  originY: 0.5
+                }}
+                className={cn(
+                  "flex flex-col gap-2 whitespace-pre-wrap p-4",
+                  chatMessage.chatParticipantId === chatParticipantUserId
+                    ? "items-end"
+                    : "items-start"
+                )}
+              >
+                <div
+                  className={cn(
+                    "flex flex-col rounded-md p-3",
+                    chatMessage.chatParticipantId === chatParticipantUserId
+                      ? "bg-blue-100"
+                      : "bg-accent"
+                  )}
+                >
+                  <span>{chatMessage.content}</span>
+                  <span className="mt-1 flex items-center gap-1 text-xs text-gray-600">
+                    <Icons.checkcheck className="h-4 w-4" />
+                    {formatDate(chatMessage.createdAt.toString())}
+                  </span>
+                </div>
+              </motion.div>
+            );
+          })}
+        </AnimatePresence>
       </div>
     </div>
   );
--- src/utils/time.tsx
+++ src/utils/time.tsx
@@ -13,3 +13,15 @@ export function getCurrentDateTime(): string {
 
   return now.toLocaleString("en-US", options);
 }
+
+export const formatDate = (dateString: string) => {
+  const date = new Date(dateString);
+  const options: Intl.DateTimeFormatOptions = {
+    day: "2-digit",
+    month: "short",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit"
+  };
+  return date.toLocaleDateString("en-US", options);
+};
