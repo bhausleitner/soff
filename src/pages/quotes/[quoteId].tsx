3315c72cebcb46a363c1ee09f1e1c2677c29918a
raban <rabanspiegel@gmail.com>
2024-07-23 17:59:05 -0700

    initial quote detail

--- src/pages/quotes/[quoteId].tsx
+++ src/pages/quotes/[quoteId].tsx
@@ -1,27 +1,20 @@
 import { useRouter } from "next/router";
-import React, { useState } from "react";
-import { ReloadIcon } from "@radix-ui/react-icons";
-import { SupplierInfo } from "~/components/supplier-detail/SupplierInfo";
-import { SupplierTabs } from "~/components/supplier-detail/SupplierTabs";
+import React from "react";
+// import { SupplierInfo } from "~/components/supplier-detail/SupplierInfo";
 import { api } from "~/utils/api";
-import { SupplierBreadcrumb } from "~/components/supplier-detail/SupplierBreadcrumb";
 import Spinner from "~/components/spinner";
-import { Button } from "~/components/ui/button";
-import { PaperPlaneIcon } from "@radix-ui/react-icons";
+import { QuoteBreadcrumb } from "~/components/quote-table/QuoteBreadcrumb";
 
-const SupplierPage = () => {
+const QuotePage = () => {
   const router = useRouter();
-  const supplierId = parseInt(router.query.supplierId as string, 10);
-  const [isCreatingRFQ, setIsCreatingRFQ] = useState(false);
+  const quoteId = parseInt(router.query.quoteId as string, 10);
 
-  const createChat = api.chat.createChat.useMutation();
-
-  if (isNaN(supplierId)) {
+  if (isNaN(quoteId)) {
     return <Spinner />;
   }
 
-  const { data, isLoading, error } = api.supplier.getSupplierById.useQuery({
-    supplierId
+  const { data, isLoading, error } = api.quote.getQuoteById.useQuery({
+    quoteId
   });
 
   if (isLoading) {
@@ -33,43 +26,18 @@ const SupplierPage = () => {
   }
 
   if (!data) {
-    return <p>No supplier found.</p>;
+    return <p>No quote found.</p>;
   }
 
   return (
     <div className="flex-1 space-y-4 p-4 pt-6 md:p-8">
       <div className="flex items-center justify-between">
-        <SupplierBreadcrumb name={data.name} />
-        <div className="flex items-center">
-          <Button
-            className="w-32"
-            onClick={async () => {
-              setIsCreatingRFQ(true);
-
-              const chatId = await createChat.mutateAsync({
-                supplierId: data.id
-              });
-
-              await router.push(`/rfq/${chatId}`);
-              setIsCreatingRFQ(false);
-            }}
-            disabled={isCreatingRFQ}
-          >
-            {isCreatingRFQ ? (
-              <ReloadIcon className="h-4 w-4 animate-spin" />
-            ) : (
-              <>
-                Start RFQ
-                <PaperPlaneIcon className="ml-2 h-4 w-4" />
-              </>
-            )}
-          </Button>
-        </div>
+        <QuoteBreadcrumb />
+        <div className="flex items-center"></div>
       </div>
-      <SupplierInfo data={data} />
-      <SupplierTabs supplierId={data.id} />
+      {/* <SupplierInfo data={data} /> */}
     </div>
   );
 };
 
-export default SupplierPage;
+export default QuotePage;
