74ab8f2c02a9df23e5e11e015d4f4ea12e905235
Berni <behausleitner@gmail.com>
2024-07-28 21:40:22 -0700

    aws update

--- src/server/s3/config.ts
+++ src/server/s3/config.ts
@@ -4,7 +4,7 @@ import AWS from "aws-sdk";
 AWS.config.update({
   region: process.env.AWS_REGION,
   credentials: new AWS.Credentials(
-    process.env.AWS_ACCESS_KEY!,
+    process.env.AWS_NON_SECRET_ACCESS_KEY!,
     process.env.AWS_SECRET_ACCESS_KEY!
   )
 });
