aa98a0e1a39a7604764a2a00d2a9a092b5642acd
raban <rabanspiegel@gmail.com>
2024-07-31 18:18:25 -0700

    consistent width of download icon

--- src/components/chat/file-badge.tsx
+++ src/components/chat/file-badge.tsx
@@ -26,12 +26,12 @@ export function FileBadge({
       onClick={handleOpen}
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
-      className="flex max-w-[170px] cursor-pointer items-center space-x-2 border border-gray-300 bg-white px-2 py-2 text-gray-700 shadow-sm transition-all duration-200 ease-in-out hover:bg-gray-100"
+      className="flex max-w-[170px] cursor-pointer items-center space-x-2 border border-gray-300 bg-white px-3 py-2 text-gray-700 transition-colors duration-200 ease-in-out hover:bg-gray-100"
     >
-      <div className="relative flex h-5 w-5 items-center justify-center">
+      <div className="flex h-5 w-5 flex-shrink-0 items-center justify-center">
         {isHovered && handleDownload ? (
           <div
-            className="absolute inset-0 flex items-center justify-center rounded-full bg-gray-200"
+            className="flex h-full w-full items-center justify-center rounded-full bg-gray-200"
             onClick={(e) => {
               e.stopPropagation();
               handleDownload();
@@ -46,18 +46,9 @@ export function FileBadge({
           <Icons.paperClip size={15} className="text-gray-500" />
         )}
       </div>
-      <span className="truncate">{fileName}</span>
+      <span className="flex-grow truncate">{fileName}</span>
       {handleRemove && (
-        <Link
-          href="#"
-          className={cn(
-            "shrink-0 dark:bg-muted dark:text-muted-foreground dark:hover:bg-muted dark:hover:text-white"
-          )}
-          onClick={(e) => {
-            e.stopPropagation();
-            handleRemove();
-          }}
-        >
+        <div className="flex h-5 w-5 flex-shrink-0 items-center justify-center">
           {uploading ? (
             <Icons.loaderCircle
               size={15}
@@ -67,9 +58,13 @@ export function FileBadge({
             <Icons.trash
               size={15}
               className="text-gray-500 hover:text-red-500"
+              onClick={(e) => {
+                e.stopPropagation();
+                handleRemove();
+              }}
             />
           )}
-        </Link>
+        </div>
       )}
     </Badge>
   );
