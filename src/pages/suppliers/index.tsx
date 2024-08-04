cfbd78e8a0cf9a2602b6b5c6f2bd9e1793fda985
raban <rabanspiegel@gmail.com>
2024-08-04 15:32:54 -0700

    Added 'Supplier +' Button!

--- src/pages/suppliers/index.tsx
+++ src/pages/suppliers/index.tsx
@@ -6,14 +6,30 @@ import {
   useGetAllSuppliers
 } from "~/constants/tableConfigs";
 import { useUser } from "@clerk/nextjs";
+import { Button } from "~/components/ui/button";
+import { Icons } from "~/components/icons";
+import { useState } from "react";
 
 export default function Supplier() {
   const user = useUser();
   const clerkUserId = user.user?.id;
+  const [isOnboarding, setIsOnboarding] = useState(false);
+
   return (
     <>
       <div className="space-y-4">
-        <SupplierBreadcrumb />
+        <div className="flex items-center justify-between">
+          <SupplierBreadcrumb />
+          <Button
+            onClick={async () => {
+              setIsOnboarding(true);
+            }}
+            variant="blue"
+          >
+            Supplier
+            <Icons.add className="ml-2 h-4 w-4" />
+          </Button>
+        </div>
         {clerkUserId && (
           <GenericTable<Supplier, { clerkUserId: string }>
             tableConfig={supplierTableConfig}
