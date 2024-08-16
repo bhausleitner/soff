d9c445ff7956b3ddcb8ffda4cbc17a73470b8ffd
Berni <behausleitner@gmail.com>
2024-08-15 18:56:07 -0700

    update

--- src/pages/quotes/compare.tsx
+++ src/pages/quotes/compare.tsx
@@ -13,7 +13,6 @@ import React, { useEffect, useState } from "react";
 import Spinner from "~/components/spinner";
 import { type QuoteComparison } from "~/utils/quote-helper";
 import { Separator } from "~/components/ui/separator";
-import { DiffBadge } from "~/components/common/DiffBadge";
 
 export default function Compare() {
   const router = useRouter();
@@ -40,13 +39,14 @@ export default function Compare() {
 
   return (
     <div className="flex h-full flex-col">
+      {/* Header section */}
       <div className="flex-shrink-0">
         <div className="flex items-center justify-between pb-4">
           <QuoteBreadcrumb quoteIds={idArray} />
         </div>
         {!isMetaDataLoading && (
           <div
-            className={`grid grid-cols-${columnCount} gap-4`}
+            className={`grid gap-4`}
             style={{
               gridTemplateColumns: `repeat(${columnCount}, minmax(150px, 1fr))`
             }}
@@ -64,13 +64,14 @@ export default function Compare() {
         )}
       </div>
 
+      {/* Body section */}
       {isMetaDataLoading ? (
         <Spinner />
       ) : (
         <div className="flex-grow overflow-hidden">
           <div className="h-full overflow-x-auto">
             <div
-              className={`grid grid-cols-${columnCount} h-full min-w-[600px]`}
+              className="h-full min-w-[600px]"
               style={{
                 gridTemplateColumns: `repeat(${columnCount}, minmax(150px, 1fr))`
               }}
@@ -97,10 +98,9 @@ export default function Compare() {
                     ))}
                 </div>
               ) : (
-                // Scrollable container start
                 <div className="col-span-full h-full overflow-y-auto py-4">
                   <div
-                    className={`grid grid-cols-${columnCount} gap-4`}
+                    className="grid gap-4"
                     style={{
                       gridTemplateColumns: `repeat(${columnCount}, minmax(150px, 1fr))`
                     }}
@@ -108,7 +108,7 @@ export default function Compare() {
                     {quotesBodyData?.map((lineItem, index: number) => (
                       <React.Fragment key={index}>
                         <div
-                          className={`col-span-full grid grid-cols-${columnCount} group rounded-xl hover:bg-gray-50`}
+                          className="group col-span-full grid rounded-xl hover:bg-gray-50"
                           style={{
                             gridTemplateColumns: `repeat(${columnCount}, minmax(150px, 1fr))`
                           }}
@@ -118,57 +118,78 @@ export default function Compare() {
                               {lineItem.lineItemDescription?.slice(0, 50)}
                             </span>
                           </div>
-                          {lineItem.quotes.map((quote, quoteIndex) => (
-                            <Popover key={quoteIndex}>
-                              <PopoverTrigger asChild>
-                                <div
-                                  className="flex items-center justify-end rounded-xl p-4 transition-colors duration-150 hover:bg-gray-100 group-hover:hover:bg-gray-100"
-                                  onMouseEnter={(e) => e.currentTarget.click()}
-                                  onMouseLeave={(e) => e.currentTarget.click()}
-                                >
-                                  {/* <DiffBadge number={12} /> */}
-                                  <span className="text-heading-3 font-heading-3 text-default-font text-right">
-                                    {formatCurrency(
-                                      quote.price / quote.quantity
-                                    )}
-                                  </span>
-                                </div>
-                              </PopoverTrigger>
-                              <PopoverContent className="w-56">
-                                <div className="text-xs text-gray-600">
-                                  <ul className="m-0 list-none p-0">
-                                    <li className="flex justify-between">
-                                      <span>Quantity:</span>
-                                      <span>{quote.quantity}</span>
-                                    </li>
-                                    <li className="flex justify-between">
-                                      <span>Unit Price:</span>
-                                      <span>
-                                        {formatCurrency(
-                                          quote.price / quote.quantity
-                                        )}
-                                      </span>
-                                    </li>
-                                    <li className="flex justify-between font-semibold">
-                                      <span>Total Price:</span>
-                                      <span>{formatCurrency(quote.price)}</span>
-                                    </li>
-                                  </ul>
-                                </div>
-                              </PopoverContent>
-                            </Popover>
-                          ))}
+                          {quotesMetaData?.map((metaQuote) => {
+                            const quote = lineItem.quotes.find(
+                              (q) => q.quoteId === metaQuote.id
+                            );
+                            return (
+                              <Popover key={metaQuote.id}>
+                                <PopoverTrigger asChild>
+                                  <div
+                                    className="flex items-center justify-end rounded-xl p-4 transition-colors duration-150 hover:bg-gray-100 group-hover:hover:bg-gray-100"
+                                    onMouseEnter={(e) =>
+                                      e.currentTarget.click()
+                                    }
+                                    onMouseLeave={(e) =>
+                                      e.currentTarget.click()
+                                    }
+                                  >
+                                    <span className="text-heading-3 font-heading-3 text-default-font text-right">
+                                      {quote
+                                        ? formatCurrency(
+                                            quote.price / quote.quantity
+                                          )
+                                        : "N/A"}
+                                    </span>
+                                  </div>
+                                </PopoverTrigger>
+                                {quote && (
+                                  <PopoverContent className="w-56">
+                                    <div className="text-xs text-gray-600">
+                                      <ul className="m-0 list-none p-0">
+                                        <li className="flex justify-between">
+                                          <span>Quantity:</span>
+                                          <span>
+                                            {quote?.quantity ?? "N/A"}
+                                          </span>
+                                        </li>
+                                        <li className="flex justify-between">
+                                          <span>Unit Price:</span>
+                                          <span>
+                                            {quote
+                                              ? formatCurrency(
+                                                  quote.price / quote.quantity
+                                                )
+                                              : "N/A"}
+                                          </span>
+                                        </li>
+                                        <li className="flex justify-between font-semibold">
+                                          <span>Total Price:</span>
+                                          <span>
+                                            {quote
+                                              ? formatCurrency(quote.price)
+                                              : "N/A"}
+                                          </span>
+                                        </li>
+                                      </ul>
+                                    </div>
+                                  </PopoverContent>
+                                )}
+                              </Popover>
+                            );
+                          })}
                         </div>
                       </React.Fragment>
                     ))}
                   </div>
                 </div>
-                // Scrollable container end
               )}
             </div>
           </div>
         </div>
       )}
+
+      {/* Footer section */}
       {!isMetaDataLoading && (
         <>
           <div className="flex-shrink-0 pb-4">
@@ -176,7 +197,7 @@ export default function Compare() {
           </div>
           <div className="flex-shrink-0">
             <div
-              className={`grid grid-cols-${columnCount} gap-4`}
+              className="grid gap-4"
               style={{
                 gridTemplateColumns: `repeat(${columnCount}, minmax(150px, 1fr))`
               }}
